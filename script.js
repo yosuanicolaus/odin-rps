cad39c5a22ca50ea9b54ea0b34e5403f435e67c0
Yosua Nicolaus <cryodawn257@gmail.com>
2022-01-20 09:07:11 +0700

    basic rps gameplay completed. you can play it now.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let playerScore = 0;
 let computerScore = 0;
 let r;
 
-let playerSelection = rps[0]; //for now, let's just hardcode this. later- make function that asks for input in console.
-let computerSelection = computerPlay();
+let playerSelection;
+let computerSelection;
 
 function computerPlay(){
     let randNum = Math.floor(Math.random() * 3);
@@ -32,7 +32,11 @@ function playRound(playerSelection, computerSelection, r){
         if (computerSelection == rps[0]) {r = result[0]}
         else if (computerSelection == rps[1]) {r = result[1]}
         else if (computerSelection == rps[2]) {r = result[2]}
+    } else {
+        return `your input isn't either rock, paper, or scissors!\ncheck your spelling!`
     }
+
+
     if (r == result[0]) {computerScore += 1}
     else if (r == result[1]) {playerScore += 1}
 
@@ -40,14 +44,16 @@ function playRound(playerSelection, computerSelection, r){
 }
 
 function game(playerSelection, computerSelection){
-    for(let i = 0; i < 5; i++){
+    //keep playing the game until either player or computer reach 3 score.
+    for(playerScore, computerScore; playerScore < 3 && computerScore < 3; playerScore, computerScore){
         console.log(playRound(playerSelection, computerSelection));
     }
 
     //check who wins
-    if (playerScore > computerScore) return `You Win`
-    else if (computerScore > playerScore) return 'You lose'
-    else return `Tie`
+    let finalResult;
+    (playerScore > computerScore) ? finalResult = `You Win` : finalResult = `You Lose`
+
+    return `Final Result : ${finalResult}`
 }
 
 
