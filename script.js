2897d8adf859ee4b2e263820690c1875cfc9c7ad
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-02 16:14:33 +0700

    rps prototype down, fully playable on browser now

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ let r;
 let playerSelection;
 let computerSelection;
 
+const buttons = document.querySelectorAll('button');
+const text = document.querySelector('div');
+
+const d1 = document.getElementById('d1');
+const d2 = document.getElementById('d2');
+const d3 = document.getElementById('d3');
+
 function computerPlay(){
     let randNum = Math.floor(Math.random() * 3);
     return rps[randNum];
@@ -32,14 +39,16 @@ function playRound(buttonID, computerSelection, r){
         if (computerSelection == rps[0]) {r = result[0]}
         else if (computerSelection == rps[1]) {r = result[1]}
         else if (computerSelection == rps[2]) {r = result[2]}
-    } else {
-        return `your input isn't either rock, paper, or scissors!\ncheck your spelling!`
-    }
+    } else return "error, please try again."
 
     if (r == result[0]) {computerScore += 1}
     else if (r == result[1]) {playerScore += 1}
 
-    return `Player - ${playerSelection} vs ${computerSelection} - Computer \nResult : ${r}\nScoreboard : Player ${playerScore} || ${computerScore} Computer\n`;
+    d1.textContent = `Player - ${playerSelection} vs ${computerSelection} - Computer`;
+    d2.textContent = `Result : ${r}`;
+    d3.textContent = `Scoreboard : Player ${playerScore} || ${computerScore} Computer`;
+
+    return `Player - ${playerSelection} vs ${computerSelection} - Computer \r\nResult : ${r}\r\nScoreboard : Player ${playerScore} || ${computerScore} Computer\n`;
 }
 
 function game(playerSelection, computerSelection){
@@ -56,10 +65,8 @@ function game(playerSelection, computerSelection){
 }
 
 
-const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playRound(button.id));
-    });
-});
+        playRound(button.id);
+    })});
