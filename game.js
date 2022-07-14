a00f5bc59c10e9476f56c7def5d5ac27227588c2
Yosua Nicolaus <cryodawn257@gmail.com>
2022-07-14 18:04:58 +0700

    create AI choice algorithm

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-const playerScore = document.getElementById("player-score");
-const playerName = document.getElementById("player-name");
-const AIScore = document.getElementById("AI-score");
-const AIName = document.getElementById("AI-name");
+const DOMplayerScore = document.getElementById("player-score");
+const DOMplayerName = document.getElementById("player-name");
+const DOMAIName = document.getElementById("AI-name");
+const DOMAIScore = document.getElementById("AI-score");
 
 const cardPage = document.getElementById("card-page");
 const battlePage = document.getElementById("battle-page");
@@ -19,6 +19,12 @@ const AICardName = document.getElementById("AI-card-name");
 const battleInfo = document.getElementById("battle-info");
 const btnNextRound = document.getElementById("next-round");
 
+DOMplayerName.textContent = localStorage.getItem("playerName");
+DOMAIName.textContent = localStorage.getItem("AIName");
+let playerScore = 0;
+let AIScore = 0;
+let AILevel = Number(localStorage.getItem("AIName").slice(-1));
+
 // for debugging
 document.onkeydown = (e) => {
   if (e.key === "f") {
@@ -34,3 +40,57 @@ document.onkeydown = (e) => {
     }
   }
 };
+
+function randomChoice() {
+  const r = Math.random();
+  if (r < 1 / 3) {
+    return "r";
+  } else if (r < 2 / 3) {
+    return "p";
+  } else {
+    return "s";
+  }
+}
+
+function getWinChoice(playerChoice) {
+  switch (playerChoice) {
+    case "r":
+      return "p";
+    case "p":
+      return "s";
+    case "s":
+      return "r";
+  }
+}
+
+function getLoseChoice(playerChoice) {
+  switch (playerChoice) {
+    case "r":
+      return "s";
+    case "p":
+      return "r";
+    case "s":
+      return "p";
+  }
+}
+
+function generateAIChoice(AILevel, playerChoice) {
+  switch (AILevel) {
+    case 0:
+      if (Math.random() > 0.5) {
+        return getLoseChoice(playerChoice);
+      } else {
+        return randomChoice();
+      }
+    case 1:
+      return randomChoice();
+    case 2:
+      if (Math.random() > 0.75) {
+        return getWinChoice(playerChoice);
+      } else {
+        return randomChoice();
+      }
+    case 3:
+      return getWinChoice(playerChoice);
+  }
+}
